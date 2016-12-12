35c14fd0453bfad5295ebc0c2e846ace434e8faf
Jamie Connelly <jamiebruceconnelly@gmail.com>
2016-12-12 14:56:38 +0000

    setup docker container for flask

---
+++ python-app/app.py
@@ -0,0 +1,29 @@
+import os
+import logging
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['GET'])
+def query_sentiment():
+    try:
+        req_json = request.get_json()
+
+        if req_json is None:
+            return jsonify(error='this service require A JSON')
+
+    except Exception as ex:
+        app.log.error(type(ex))
+        app.log.error(ex.args)
+        app.log.error(ex)
+        return jsonify(error=str(ex))
+
+if __name__ == '__main__':
+    LOG_FORMAT = "'%(asctime)s - %(name)s - %(levelname)s - %(message)s'"
+    logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+    app.log = logging.getLogger(__name__)
+    port = os.environ['FLASK_PORT']
+    app.run(host='0.0.0.0', port=int(port), debug=False)
