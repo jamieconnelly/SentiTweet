5831d3bbe49923deeea644bbc23580d6c38d8bc0
jamieconnelly <jamiebruceconnelly@gmail.com>
2017-01-08 16:02:49 +0000

    Replace emojis with meaning
    - added tests for tokenisor class
    - change smiley/sad emojis to happy/sad meanings

---
+++ backend/tests/test_preprocessor.py
@@ -0,0 +1,52 @@
+from utils.preprocessor import Preprocessor
+import unittest
+
+
+class TestPreProcessor(unittest.TestCase):
+
+    def setUp(self):
+        self.pre = Preprocessor()
+
+    def tearDown(self):
+        self.pre = None
+
+    def test_tokeniser(self):
+        tweet = 'split tweet tokens'
+        self.assertEqual(self.pre.token(tweet), ['split', 'tweet', 'tokens'])
+
+    def test_remove_stopwords(self):
+        tweet = 'this has a lot of stopwords'
+        self.assertEqual(self.pre.token(tweet), ['lot', 'stopwords'])
+
+    def test_minimise_repeated_chars(self):
+        tweet = 'yaaaaaaaaaaay'
+        self.assertEqual(self.pre.token(tweet), ['yaaay'])
+
+    def test_URL_replacement(self):
+        tweet = 'http://www.example'
+        self.assertEqual(self.pre.token(tweet), ['URL'])
+
+    def test_lowercase(self):
+        tweet = 'CHANGE TWEET LOWER'
+        self.assertEqual(self.pre.token(tweet), ['change', 'tweet', 'lower'])
+
+    def test_remove_punctuation(self):
+        tweet = 'test, punct - removal!!!!?.'
+        self.assertEqual(self.pre.token(tweet), ['test', 'punct', 'removal'])
+
+    def test_replace_happy_emojis(self):
+        tweet = u'\U0001f601 \U0001F60F \U0001F618 \
+                 \U0001F61D \U0001F61A \U0001F638 \U0001F63D \U0001F60E'
+        self.assertEqual(self.pre.token(tweet),
+                         ['happy', 'happy', 'happy', 'happy', 'happy',
+                         'happy', 'happy', 'happy'])
+
+    def test_replace_sad_emojis(self):
+        tweet = u'\U0001F612 \U0001F616 \U0001F615 \U0001F61E \U0001F62B \
+                 \U0001F63E \U0001F63F' 
+        self.assertEqual(self.pre.token(tweet),
+                         ['sad', 'sad', 'sad', 'sad', 'sad', 'sad', 'sad'])
+
+
+if __name__ == '__main__':
+    unittest.main()
--- backend/utils/preprocessor.py
+++ backend/utils/preprocessor.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import string
 import re
+import sys
 
 from nltk.corpus import stopwords as sw
 from nltk.corpus import wordnet as wn
@@ -15,14 +16,15 @@ from sklearn.base import BaseEstimator, TransformerMixin
 class Preprocessor(BaseEstimator, TransformerMixin):
 
     def __init__(self):
-        self.stopwords  = sw.words('english')
-        self.punct      = set(string.punctuation)
+        self.stopwords = sw.words('english')
+        self.punct = set(string.punctuation)
         self.lemmatizer = WordNetLemmatizer()
-        self.table      = {ord(char): None for char in string.punctuation}
-        self.emoji      = self.emoji_patterns()
-        self.tknzr      = TweetTokenizer(preserve_case=True,
-                                         reduce_len=True,
-                                         strip_handles=True)
+        self.emoji_happy = self.emoji_happy()
+        self.emoji_sad = self.emoji_sad()
+        self.tknzr = TweetTokenizer(preserve_case=True,
+                                    reduce_len=True,
+                                    strip_handles=True)
+        # sys.maxunicode == 0xffff
 
     def fit(self, X, y=None):
         return self
@@ -31,23 +33,64 @@ class Preprocessor(BaseEstimator, TransformerMixin):
         return X
 
     def transform(self, X):
-        return [list(self.tokenize(tweet)) for tweet in X]
+        return [list(self.token(tweet)) for tweet in X]
+
+    def emoji_happy(self):
+        try:
+            return re.compile(u'['
+                              u'\U0001f600-\U0001F60F'
+                              u'\U0001F617-\U0001F61D'
+                              u'\U0001F638-\U0001F63D'
+                              ']+', re.UNICODE)
+        except re.error:
+            return re.compile(u'('
+                              u'\ud83d[\ude00-\ude0f]|'
+                              u'\ud83d[\ude17-\ude1d]|'
+                              u'\ud83d[\ude38-\ude3d]'
+                              ')+', re.UNICODE)
+
+    def emoji_sad(self):
+        try:
+            return re.compile(u'['
+                              u'\U0001F612-\U0001F616'
+                              u'\U0001F61E-\U0001F62B'
+                              u'\U0001F63E-\U0001F63F'
+                              ']+', re.UNICODE)
+        except re.error:
+            return re.compile(u'('
+                              u'\ud83d[\ude12-\ude16]|'
+                              u'\ud83d[\ude1e-\ude2b]|'
+                              u'\ud83d[\ude3e-\ude3f]'
+                              ')+', re.UNICODE)
 
     def emoji_patterns(self):
         try:
-            # Wide UCS-4 build
             return re.compile(u'['
                               u'\U0001F300-\U0001F64F'
                               u'\U0001F680-\U0001F6FF'
                               u'\u2600-\u26FF\u2700-\u27BF]+',
                               re.UNICODE)
         except re.error:
-            # Narrow UCS-2 build
             return re.compile(u'('
                               u'\ud83c[\udf00-\udfff]|'
                               u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'
                               u'[\u2600-\u26FF\u2700-\u27BF])+',
                               re.UNICODE)
+
+    def token(self, tweet):  
+        tweet = self.emoji_happy.sub(r'happy', tweet)
+        tweet = self.emoji_sad.sub(r'sad', tweet)
+        result = []
+
+        for token in self.tknzr.tokenize(tweet):
+            token = token.lower()
+            token = token.strip()
+            token = re.sub(r'http\S+', 'URL', token)
+            if token in self.stopwords or token in self.punct:
+                continue
+            result.append(token)
+
+        return result
 
     def tokenize(self, tweet):
 
@@ -55,10 +98,10 @@ class Preprocessor(BaseEstimator, TransformerMixin):
             token = token.lower()
             token = token.strip()
             token = token.translate(self.table)
-            token = re.sub(r'http\S+', '', token)
+            token = re.sub(r'http\S+', 'URL', token)
             token = self.emoji.sub(r'', token)
 
-            if token in self.stopwords:
+            if token in self.stopwords or token in self.punct:
                 continue
 
             lemma = self.lemmatize(token, tag)
